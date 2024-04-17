0961d2e80cb1620b5e0c803958e8a45fce646fee
Gonzalo Chimento <gonzalochimento@ibm.com>
2024-04-17 17:11:48 -0300

    test bug fixes

--- scripts/apic_platform_publish_to_catalog.py
+++ scripts/apic_platform_publish_to_catalog.py
@@ -151,11 +151,11 @@ def orchestrate():
                                                                     var_bearer_token)
                 # print("publish_resp: ",publish_resp)
                 if "errorresponse" in publish_resp:
-                    apic_publish_audit[product_file_name] = "FAILED" + publish_resp['errorresponse']
+                    apic_publish_audit[product_file_name] = "FAILED " + str(publish_resp['errorresponse'])
                 elif "state" in publish_resp:
                     apic_publish_audit[product_file_name] = "SUCCESS"
                 else:
-                    apic_publish_audit[product_file_name] = "FAILED" + publish_resp['errorresponse']
+                    apic_publish_audit[product_file_name] = "FAILED " + str(publish_resp['errorresponse'])
             
             print(INFO + "apic_publish_audit: ",apic_publish_audit)
             Audit_res.update_apic_publish_audit(WORKING_DIR_BASIC, apic_publish_audit)
